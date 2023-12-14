d2bb474da7a0b645541bbffd9a37e33cc5752624
Joaquín Díaz <mjoaquind@gmail.com>
2023-12-14 13:22:22 -0300

    avances primera practica integradora

--- src/routes/dbProducts.router.js
+++ src/routes/dbProducts.router.js
@@ -12,9 +12,14 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/:pid', async (req, res) => {
+    const product = await productsModel.findOne({ _id: req.params.pid });
+    res.send({product});
+})
+
 router.post('/', async (req, res) => {
     try {
-        let {
+        const {
             title,
             description,
             price,
@@ -45,6 +50,49 @@ router.post('/', async (req, res) => {
     } catch (error) {
         res.status(400).send({ status: "error", message: error.message });
     }
+})
+
+router.get('/insert', async (req, res) => {
+    const result = await productsModel.insertMany(products)
+    res.send({result});
+})
+
+router.put('/:pid', async (req, res) => {
+    const id = req.params.pid;
+    const {
+        title,
+        description,
+        price,
+        thumbnail = [],
+        code,
+        stock,
+        status = true,
+        category
+    } = req.body;
+
+    if (!title || !description || !price || !category || !code || !stock) {
+        return res.status(400).send({ status: "error", message: "All fields are required" });
+    }
+
+    const product = {
+        title,
+        description,
+        price,
+        thumbnail,
+        code,
+        stock,
+        status,
+        category
+    }
+
+    const result = await productsModel.updateOne({_id: id}, {$set: product});
+    res.send({result});
+})
+
+router.delete('/:pid', async (req, res) => {
+    const id = req.params.pid;
+    let result = await productsModel.deleteOne({_id: id});
+    res.send({result});
 })
 
 export default router;
