bdafe917aa1f31a962a1541be28fcb5e8de026d2
Joaquín Díaz <mjoaquind@gmail.com>
2024-01-14 01:00:43 -0300

    finalizada entrega clase 21

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -73,8 +73,9 @@ const initializePassport = () =>{
             const user = await userManager.getByEmail(profile._json.email);
             if(user){
                 console.log('User already exists');
-                console.log(user);
                 return done(null, user)
+                // return done(null, false);
+                // si dejo en false no me redirecciona a la pagina principal
             }
             const newUser = {
                 first_name,
