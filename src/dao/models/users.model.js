60cc5caacaaac22559c6c93dd54b48016e1fea57
Joaquín Díaz <mjoaquind@gmail.com.com>
2024-01-21 11:30:38 -0300

    avances segunda práctica integradora

--- src/dao/models/users.model.js
+++ src/dao/models/users.model.js
@@ -5,10 +5,25 @@ const usersCollection = 'users';
 const usersSchema = new mongoose.Schema({
     first_name: { type: String },
     last_name: { type: String },
-    email: { type: String },
+    email: { type: String, unique: true, required: true },
     age: { type: Number },
-    password: { type: String }
+    password: { type: String },
+    carts: {
+        type: [{
+            cart: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'carts',
+                required: true
+            }
+        }],
+        default: []
+    },
+    role: { type: String, default: 'user' }
 });
+
+cartsSchema.pre('findOne', function () {
+    this.populate('carts.cart');
+})
 
 const usersModel = mongoose.model(usersCollection, usersSchema);
 
