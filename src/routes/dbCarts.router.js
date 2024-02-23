b9eb23d7f48e20d57d357baf4a7dd2f835c01567
Joaquín Díaz <mjoaquind@gmail.com>
2024-02-23 09:52:02 -0300

    avances tercera pre-entrega

--- src/routes/dbCarts.router.js
+++ src/routes/dbCarts.router.js
@@ -8,18 +8,18 @@ router.get('/', CartController.getCarts);
 
 router.get('/:cid', CartController.getCartById);
 
-router.post('/', checkRole('user'), checkRole('user'), CartController.createCart);
+router.post('/', CartController.createCart);
 
 router.post('/:cid/products/:pid', checkRole('user'), CartController.addProductToCart);
 
-router.put('/:cid/products/:pid', checkRole('user'), CartController.updateProductQuantity);
+router.put('/:cid/products/:pid', CartController.updateProductQuantity);
 
-router.put('/:cid', checkRole('user'), CartController.updateCart)
+router.put('/:cid', CartController.updateCart)
 
-router.delete('/:cid', checkRole('user'), CartController.deleteCart);
+router.delete('/:cid', CartController.deleteCart);
 
-router.delete('/:cid/products/:pid', checkRole('user'), checkRole('user'), CartController.deleteProductFromCart);
+router.delete('/:cid/products/:pid', CartController.deleteProductFromCart);
 
-router.post('/:cid/purchase', checkRole('user'), CartController.purchaseCart);
+router.post('/:cid/purchase', CartController.purchaseCart);
 
 export default router;
