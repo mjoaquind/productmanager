082241a958b0f7eac82f8cf572264b945cfe4914
Joaquín Díaz <mjoaquind@gmail.com.com>
2024-04-15 21:05:38 -0300

    pruebas de middleware de uploader en users.router.js

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
 import { UserController } from '../controllers/users.controller.js';
+import uploader from '../utils/multer.js';
 
 const router = Router();
 
 router.put('/premium/:uid', UserController.changeRole);
-router.post('/:uid/documents', UserController.addDocuments);
+router.post('/:uid/documents',uploader.fields([
+    { name: 'identity'},
+    { name: 'address'},
+    { name: 'account'},
+    { name: 'products'},
+    { name: 'profile'}
+]), UserController.addDocuments);
 
 export default router;
