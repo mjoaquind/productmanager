e10f74f2fa2418310cfe0a356c53cd4e803121a6
Joaquín Díaz <mjoaquind@gmail.com.com>
2024-04-19 19:48:18 -0300

    modificaciones en addDocuments

--- src/utils/multer.js
+++ src/utils/multer.js
@@ -20,7 +20,7 @@ const storage = multer.diskStorage({
         }
     },
     filename: function(req, file, cb) {
-        cb(null, `${file.fieldname}-${Date.now()}`);
+        cb(null, `${file.fieldname}-${Date.now()}-${file.originalname}`);
     }
 });
 
