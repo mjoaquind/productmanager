608a1db21201dadd88bb928beeb3bc06fdfc3b62
Joaquín Díaz <joaquin.diaz@uner.edu.ar>
2024-05-01 12:16:01 -0300

    arreglos en users

--- src/public/js/users.js
+++ src/public/js/users.js
@@ -3,7 +3,6 @@ const deleteUser = async (id) => {
         const response = await fetch(`/api/users/${id}`, {
             method: 'DELETE'
         });
-        console.log(response);
         if(response.status === 200) {
             Toastify({
                 text:`${response.status} ${response.statusText} Cart ${id} is empty`,
@@ -26,3 +25,32 @@ const deleteUser = async (id) => {
         console.log(error);
     }
 };
+
+const changeRole = async (id) => {
+    try {
+        const response = await fetch(`/api/users/premium/${id}`, {
+            method: 'PUT'
+        });
+        console.log(response);
+        if(response.status === 200) {
+            Toastify({
+                text:`${response.status} ${response.statusText} User ${id} role changed`,
+                position:'right',
+                style: {
+                    background: "linear-gradient(to right, #00b09b, #96c93d)",
+                }
+            }).showToast()
+            window.location.replace('/users');
+        } else {
+            Toastify({
+                text:`${response.status} ${response.statusText}`,
+                position:'right',
+                style: {
+                    background: "linear-gradient(to right, #d9534f, #dc3545)",
+                }
+            }).showToast()
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
